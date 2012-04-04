90cf248d22e1a034ea4c99efe1a6517915424c30
Vincent Velociter <vincent.velociter@gmail.com>
2012-04-04 22:51:30 +0200

    Safe RPN calculator

--- learnyouahaskell/solveRPN.hs
+++ learnyouahaskell/solveRPN.hs
@@ -1,12 +1,21 @@
-import Data.List  
-  
-solveRPN :: String -> Float  
-solveRPN = head . foldl foldingFunction [] . words  
-    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
-            foldingFunction (x:y:ys) "+" = (x + y):ys  
-            foldingFunction (x:y:ys) "-" = (y - x):ys  
-            foldingFunction (x:y:ys) "/" = (y / x):ys  
-            foldingFunction (x:y:ys) "^" = (y ** x):ys  
-            foldingFunction (x:xs) "ln" = log x:xs  
-            foldingFunction xs "sum" = [sum xs]  
-            foldingFunction xs numberString = read numberString:xs  
+import Data.List
+import Control.Monad
+
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+    [result] <- foldM foldingFunction [] (words st)
+    return result
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = return ((x * y):ys)
+foldingFunction (x:y:ys) "+" = return ((x + y):ys)
+foldingFunction (x:y:ys) "-" = return ((y - x):ys)
+foldingFunction (x:y:ys) "/" = return ((y / x):ys)
+foldingFunction (x:y:ys) "^" = return ((y ** x):ys)
+foldingFunction (x:xs) "ln" = return (log x:xs)
+foldingFunction xs "sum" = return ([sum xs])
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x,"")] -> Just x
+                                _ -> Nothing
